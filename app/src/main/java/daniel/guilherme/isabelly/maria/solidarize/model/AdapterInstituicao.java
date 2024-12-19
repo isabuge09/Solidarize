ed97abd1246cabd50a6bc88b8282c7d1ebd7d3c0
malu <maluarnholdsampaio@gmail.com>
2024-12-19 17:09:13 -0300

    Ajustes e Estilização - 19/12

--- app/src/main/java/daniel/guilherme/isabelly/maria/solidarize/model/AdapterInstituicao.java
+++ app/src/main/java/daniel/guilherme/isabelly/maria/solidarize/model/AdapterInstituicao.java
@@ -1,5 +1,6 @@
 package daniel.guilherme.isabelly.maria.solidarize.model;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.chip.Chip;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 import daniel.guilherme.isabelly.maria.solidarize.R;
-import daniel.guilherme.isabelly.maria.solidarize.fragments.EventsFragment;
 import daniel.guilherme.isabelly.maria.solidarize.fragments.InstituicoesFragment;
+import daniel.guilherme.isabelly.maria.solidarize.activities.IntituicaoActivity; // Importe sua Activity de destino
 
-public class AdapterInstituicao extends RecyclerView.Adapter<AdapterInstituicao.EventViewHolder> {
+public class AdapterInstituicao extends RecyclerView.Adapter<AdapterInstituicao.InstituicaoViewHolder> {
 
     private List<Instituicao> instituicaoList;
     private InstituicoesFragment instituicaoFragment;
@@ -28,39 +27,44 @@ public class AdapterInstituicao extends RecyclerView.Adapter<AdapterInstituicao.
         this.instituicaoFragment = instituicaoFragment;
     }
 
-    public static class EventViewHolder extends RecyclerView.ViewHolder {
-        public EventViewHolder(@NonNull View itemView) {
+    public static class InstituicaoViewHolder extends RecyclerView.ViewHolder {
+        public InstituicaoViewHolder(@NonNull View itemView) {
             super(itemView);
         }
     }
 
     @NonNull
     @Override
-    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public InstituicaoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.instituicao_item, parent, false);
-        return new EventViewHolder(view);
+        return new InstituicaoViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull InstituicaoViewHolder holder, int position) {
         Instituicao instituicao = instituicaoList.get(position);
+
+        String imageName = "inst" + (position + 1);  // Adiciona 1 ao position, já que normalmente as imagens começam com 1
+        int imageResId = holder.itemView.getContext().getResources().getIdentifier(imageName, "drawable", holder.itemView.getContext().getPackageName());
 
         TextView tvname = holder.itemView.findViewById(R.id.institutionNameTextView);
         tvname.setText(instituicao.nome);
 
-        Chip categoria = holder.itemView.findViewById(R.id.categoryChip);
-        categoria.setText((CharSequence) instituicao.categorias);
+        Chip categoria = holder.itemView.findViewById(R.id.categoryChipInst1);
+        categoria.setText(instituicao.categorias.get(0));
 
         TextView tvdesc = holder.itemView.findViewById(R.id.institutionDescriptionTextView);
         tvdesc.setText(instituicao.descricao);
 
         ImageView imagem = holder.itemView.findViewById(R.id.institutionImageView);
-        imagem.setImageResource(R.drawable.inst1);
+        imagem.setImageResource(imageResId);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                instituicaoFragment.navegarParaDetalhesInstituicao();
+                Intent intent = new Intent(holder.itemView.getContext(), IntituicaoActivity.class);
+                intent.putExtra("INSTITUICAO_ID", instituicao.nome); // Passe qualquer dado necessário
+                holder.itemView.getContext().startActivity(intent);
             }
         });
     }
